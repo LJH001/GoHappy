cd37c07f823cab03a01486360c1ddaca22ae8ec4
liujianhui <ljh281693@126.com>
2017-01-17 23:27:56 +0800

    update

---
+++ app/src/main/java/com/example/liujianhui/gohappy/app/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.liujianhui.gohappy.app;
+
+import android.app.Application;
+import android.widget.Toast;
+
+/**
+ * Created by liujianhui on 2017/1/17.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
