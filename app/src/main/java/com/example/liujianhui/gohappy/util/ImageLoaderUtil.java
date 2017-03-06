4379399fd09b8a14982976b1a0fba7be2e5b45fe
liujianhui <ljh281693@126.com>
2017-03-06 23:56:48 +0800

    add some imageloader tools

--- app/src/main/java/com/example/liujianhui/gohappy/util/ImageLoaderUtil.java
+++ app/src/main/java/com/example/liujianhui/gohappy/util/ImageLoaderUtil.java
@@ -1,9 +1,12 @@
 package com.example.liujianhui.gohappy.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.liujianhui.gohappy.R;
 
 /**
  * Description:图片加载类 <br>
@@ -13,19 +16,109 @@ import com.bumptech.glide.Glide;
  */
 public class ImageLoaderUtil {
     /**
-     * 图片加载
-     * @param mContext 上下文
-     * @param url 图片加载地址
-     * @param resId 默认加载图片
+     * 加载非gif图片(具有默认图片)
+     *
+     * @param mContext   上下文
+     * @param url        图片加载地址
      * @param mImageView 图片显示控件
      */
-    public static void loadImage(Context mContext, String url, int resId, ImageView mImageView) {
-        Glide
-                .with(mContext)
+    public static void loadImage(Context mContext, String url, ImageView mImageView) {
+        Glide.with(mContext)
                 .load(url)
                 .centerCrop()
-                .placeholder(resId)
+                .placeholder(R.drawable.bg_trans_circle)
                 .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                 .into(mImageView);
     }
+
+    /**
+     * 不缓存，全部从网络加载图片
+     *
+     * @param context 组件对象
+     * @param url      请求图片地址
+     * @param iv       加载图片控件
+     */
+    public static void loadAll(Context context, String url, ImageView iv) {
+        Glide.with(context)
+                .load(url)
+                .crossFade()
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(iv);
+    }
+
+
+    /**
+     * 加载gif图片(不具有默认图片)
+     *
+     * @param mContext   上下文对象
+     * @param url        图片加载地址
+     * @param mImageView 图片显示控件
+     */
+    public static void loadGifImage(Context mContext, String url, ImageView mImageView) {
+        Glide.with(mContext)
+                .load(url)
+                .asGif()
+                .centerCrop()
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
+    }
+
+    /**
+     * 加载gif图片(不具有默认图片)
+     *
+     * @param mContext   上下文对象
+     * @param resId      图片加载地址
+     * @param mImageView 图片显示控件
+     */
+    public static void loadGifImage(Context mContext, int resId, ImageView mImageView) {
+        Glide.with(mContext)
+                .load(resId)
+                .asGif()
+                .centerCrop()
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
+    }
+
+    /**
+     * 加载gif图片(不具有默认图片)
+     *
+     * @param mContext   上下文对象
+     * @param url        图片加载地址
+     * @param mImageView 图片显示控件
+     * @param width      图片宽
+     * @param height     图片高
+     */
+    public static void loadGifImage(Context mContext, String url, ImageView mImageView,int width,int height) {
+        Glide.with(mContext)
+                .load(url)
+                .asGif()
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .override(width, height)
+                .into(mImageView);
+    }
+
+    /**
+     * 加载gif图片(不具有默认图片)
+     *
+     * @param mContext   上下文对象
+     * @param resId      图片加载地址
+     * @param mImageView 图片显示控件
+     * @param width      图片宽
+     * @param height     图片高
+     */
+    public static void loadGifImage(Context mContext, int resId, ImageView mImageView,int width,int height) {
+        Glide.with(mContext)
+                .load(resId)
+                .asGif()
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .override(width, height)
+                .into(mImageView);
+    }
+
 }
