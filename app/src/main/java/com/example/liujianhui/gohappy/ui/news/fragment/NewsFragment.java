2baf9b6bd1e6ab0004ec53c19aa042ee602db2ec
ljh <ljh281693@126.com>
2017-08-02 08:13:01 +0800

    add some log

--- app/src/main/java/com/example/liujianhui/gohappy/ui/news/fragment/NewsFragment.java
+++ app/src/main/java/com/example/liujianhui/gohappy/ui/news/fragment/NewsFragment.java
@@ -19,9 +19,7 @@ public class NewsFragment extends BaseFragment {
     protected void initInject() {
         getFragmentComponent().inject(this);
         Log.d("NewsFragment","NewsFragment initInject====");
-        Log.d("NewsFragment222","NewsFragment initInjec2t222222222222222====");
-        Log.d("NewsFragment222","NewsFragment initInjec2t222222222222222====");
-        Log.d("NewsFragment222","NewsFragment initInjec2t222222222222222====");
+        Log.d("NewsFragment","NewsFragment initInject2222====");
 
     }
 
